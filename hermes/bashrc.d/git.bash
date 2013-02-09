c92bd0f6706fe1f751ab46bdf1a8fad412a73f9b
Claudio Ortolina <claudio@jumpzero.com>
2013-02-09 10:27:34 +0000

    Adds misc bash functions and autocompletions.
    Signed-off-by: Claudio Ortolina <claudio@jumpzero.com>

---
+++ hermes/bashrc.d/git.bash
@@ -0,0 +1,4 @@
+alias gst='git status --short'
+alias gc='git commit'
+alias ggpull='git pull --rebase origin $(__git_branch)'
+alias ggpush='git push origin $(__git_branch)'
---
+++ hermes/bashrc.d/python.bash
@@ -0,0 +1,2 @@
+export PATH=/usr/local/share/python:$PATH
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages
